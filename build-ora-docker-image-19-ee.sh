ee83cba5fc77643494b866e3ac64d19c1eed2c3f
Lubo Sladok <bikewake@gmail.com>
2023-05-01 11:23:30 +0200

    fix builder path

--- build-ora-docker-image-19-ee.sh
+++ build-ora-docker-image-19-ee.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp ./19-ee-installer-zip-folder/LINUX.X64_193000_db_home.zip ./ora-docker-installers/OracleDatabase/SingleInstance/dockerfiles/19.3.0/
+cp ./19-ee-installer-zip-folder/LINUX.X64_193000_db_home.zip ora-docker-image-builder-scripts/OracleDatabase/SingleInstance/dockerfiles/19.3.0/
 
 ora-docker-image-builder-scripts/OracleDatabase/SingleInstance/dockerfiles/./buildContainerImage.sh -e -v 19.3.0
 
